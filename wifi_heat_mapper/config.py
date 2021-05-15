e7d8ae4c3d6f343f0b3472f460ff9cb9bab8b9be
Nischay Ram Mamidi <NischayPro@gmail.com>
2021-05-15 21:26:12 +0530

    added example floor plan

--- wifi_heat_mapper/config.py
+++ wifi_heat_mapper/config.py
@@ -208,7 +208,7 @@ def start_config(config_file):
         exit(1)
 
     while True:
-        target_interface = input("Please enter the target wireless interface to run benchmark on (example: wlan0 or wlp2s0): ")
+        target_interface = input("Please enter the target wireless interface to run benchmark on (example: wlan0): ")
         target_interface = target_interface.strip()
         if not target_interface.isalnum():
             print("Invalid interface")
